7ae575be327e6d54954599c4af65cdf6a481d932
Maxim <89240935+Maxmosk@users.noreply.github.com>
2022-01-30 00:12:27 +0300

    From int64 to string

---
+++ nasm_strdec/strdec.asm
@@ -0,0 +1,19 @@
+global _start
+
+
+section .text
+
+; Params:
+; rax: number
+; rsi: address of output
+; Return:
+; [rsi..(rsi+19)]: string with decimal number
+; rdx: lenth of string
+;(--------------------------------------
+strdec64:
+        
+;)--------------------------------------
+
+_start:
+        
+
