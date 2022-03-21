17c6a3396176bcb51ba0216c97b9c0f134b3d756
Maxmosk <maxmosk@list.ru>
2022-03-21 17:50:02 +0300

    Factorial ended!

--- nasm_factorial/main.c
+++ nasm_factorial/main.c
@@ -8,7 +8,7 @@ extern uint64_t factorial (uint64_t n);
 int main (void)
 {
     uint64_t n = 12;
-    //scanf ("%ld", &n);
+    scanf ("%ld", &n);
 
     n = factorial (n);
     printf ("%ld\n", n);
