296135045a06933a7cba4976a416ccee78716d9b
Maxmosk <maxmosk@list.ru>
2022-04-23 14:55:38 +0300

    [Improvement] Sorting on FPC started

---
+++ nasm_sort/pasfromc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+extern int SORT_$$_SORT$SMALLINT$$SMALLINT (int a);
+
+
+int main (void)
+{
+    printf("%d\n", SORT_$$_SORT$SMALLINT$$SMALLINT (1));
+    return 0;
+}
+
---
+++ nasm_sort/sort.pp
@@ -0,0 +1,15 @@
+unit sort;
+
+interface
+
+function SORT (a: integer): integer;
+
+implementation
+
+function SORT (a: integer): integer;
+begin
+    SORT := a;
+end;
+
+end.
+
