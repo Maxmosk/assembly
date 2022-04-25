1c44399d9f495943373d6d5bea903e20d9cf9b8a
Maxmosk <maxmosk@list.ru>
2022-04-25 21:08:25 +0300

    [Bug] Addition not work... Sad.

--- nasm_quadratic/main.c
+++ nasm_quadratic/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 
-extern long double _abs (long double number);
+extern double _abs (double num);
+extern double _add (double num_1, double num_2);
 
 
 int main (void)
 {
-    printf ("%Lf\n", _abs (-0.3));
+    printf ("%Lf\n", _add (-0.3, 23.0));
 
     return 0;
 }
--- nasm_quadratic/solver.asm
+++ nasm_quadratic/solver.asm
@@ -1,11 +1,28 @@
-global _abs
+global _abs, _add
 
 
 section .text
 
 _abs:
-        fld     tword [rsp + 8]
+        fld     qword [rsp + 8]
         fabs
+
+        ret
+
+
+_add:
+        mov     rax,    xmm0
+        mov     [rsp - 8],  xmm0
+        mov     [rsp - 16],  xmm1
+        
+        fld     qword [rsp - 8]
+        fld     qword [rsp - 16]
+
+        faddp
+        
+
+        fstp    [rsp - 8]
+        mov     [rsp - 8],  xmm0
 
         ret
 
