1636803f7577f888fe2d0bead921b0f58054efb1
Maxmosk <maxmosk@list.ru>
2022-05-01 22:34:48 +0300

    [Improvement] Better documentation

--- nasm_fabs/fabs.asm
+++ nasm_fabs/fabs.asm
@@ -3,6 +3,13 @@ global _fabs
 
 section .text
 
+; ======================================
+; Calculate absolute value of double floating point number
+; PARAMS
+; xmm0 - number
+; RETURN
+; xmm0 - fabs(number)
+; ======================================
 _fabs:
         pslld   xmm0,   1
         psrld   xmm0,   1
